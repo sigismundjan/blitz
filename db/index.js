a8ded99504e240c7652be69bae0b746d4faaa86a
shalahudinfadil <fadilbhakti@gmail.com>
2020-08-17 16:01:22 +0700

    fix postgres config

--- db/index.js
+++ db/index.js
@@ -4,7 +4,13 @@ dotenv.config();
 // Node PostgreSQL
 const { Pool } = require('pg');
 
-const pool = new Pool();
+const pool = new Pool({
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    user: process.env.PGUSER,
+    passowrd: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE
+});
 
 module.exports = {
     query: (text, params, callback) => {
